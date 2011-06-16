5e3574d786d6080229c17f4e1019a4d0be382590
rodiongork <rodiongork@20825f2e-348f-46e3-8257-84247e867ffe>
2011-06-16 05:00:46 +0000

    Approach to "clever" implementation of Follow. Not finished.

--- src/frej/Follow.java
+++ src/frej/Follow.java
@@ -12,6 +12,37 @@ class Follow extends Elem {
     
     @Override
     public double matchAt(int i) {
+        
+        class PartMatcher {
+            public int len;
+            public double res;
+            public StringBuilder s = new StringBuilder();
+            
+            protected double matchAtFrom(int i, int j) {
+                len = 0;
+                res = 1;
+                s.setLength(0);
+                
+                while(j < children.length) {
+                    double cur;
+                    cur = children[j].matchAt(i + len);
+                    if (children[j] instanceof Optional) {
+                        
+                    } // if
+                    res *= 1 - Math.min(cur, 1);
+                    len += children[j].getMatchLen();
+                    s.append(children[j].getReplacement());
+                } // while
+                
+                return res;
+            } // matchAtFrom
+            
+        } // class PartMatcher
+        
+        
+        
+        
+        // ==================================
         StringBuilder s = new StringBuilder();
         double worstResult = 0;
         
