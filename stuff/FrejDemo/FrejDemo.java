240be6683848c572800d1fbd83ca77bbc1fa9f41
rodiongork <rodiongork@20825f2e-348f-46e3-8257-84247e867ffe>
2011-06-23 04:36:27 +0000

    Prototype for demo applet added.

---
+++ stuff/FrejDemo/FrejDemo.java
@@ -0,0 +1,55 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+import frej.*;
+
+
+public class FrejDemo extends JApplet implements ActionListener {
+
+    
+    private JButton btnTry;
+    private JTextField txtRegex;
+    private JTextField txtInput;
+    private JLabel txtAnswer;
+    
+    
+    public FrejDemo() {
+        super();
+
+        txtRegex = new JTextField(30);
+        txtInput = new JTextField(30);
+        btnTry = new JButton("Try!");
+        txtAnswer = new JLabel("Answer");
+
+        getContentPane().setLayout(new FlowLayout());
+
+        getContentPane().add(txtRegex);
+        getContentPane().add(txtInput);
+        getContentPane().add(btnTry);
+        getContentPane().add(txtAnswer);
+        btnTry.addActionListener(this);
+
+    } // Applet
+
+
+    public void actionPerformed(ActionEvent evt) {
+        Regex r;
+        
+        try {
+            r = new Regex(txtRegex.getText());
+            if (r.match(txtInput.getText())) {
+                txtAnswer.setText(r.getReplacement());
+            } else {
+                txtAnswer.setText("// Not matched!");
+            } // else
+        } catch (Exception e) {
+            txtAnswer.setText("// Exception!");
+            return;
+        } // catch
+
+    } // actionPerformed
+
+
+} // Applet
